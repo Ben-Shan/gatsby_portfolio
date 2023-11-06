cec6cd8f2e2ae0a0c3510a65514b109fe383ea60
Ben-Shan <bnshanahan@gmail.com>
2023-11-06 19:59:49 +0000

    Window error fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,40 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
 
 const Layout = props => {
-  const { title, children, isHomepage } = props
-  const [toggleNav, setToggleNav] = React.useState(false)
+  const { title, children, isHomepage } = props;
+  const [toggleNav, setToggleNav] = useState(false);
 
+  // This function is used to scroll to the top of the page
   function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-// Listen for scroll events to show or hide the button
-window.addEventListener('scroll', function() {
-    const backToTopBtn = document.getElementById('backToTop');
-    if (window.scrollY > 2000) {
-      backToTopBtn.style.opacity = '1';
-  } else {
-      backToTopBtn.style.opacity = '0';
   }
-});
+
+  useEffect(() => {
+    // This function is responsible for toggling the visibility of the back to top button
+    function toggleBackToTopBtn() {
+      const backToTopBtn = document.getElementById('backToTop');
+      if (window.scrollY > 2000) { // This value can be adjusted based on when you want the button to appear
+        backToTopBtn.style.opacity = '1';
+        backToTopBtn.style.display = 'block'; // Make the button visible
+      } else {
+        backToTopBtn.style.opacity = '0';
+        // Set timeout to delay hiding the button to allow the opacity transition to finish
+        setTimeout(() => {
+          if (window.scrollY <= 2000) {
+            backToTopBtn.style.display = 'none';
+          }
+        }, 1000); // Adjust this to match your opacity transition time
+      }
+    }
+
+    // Add the scroll event listener when the component mounts
+    window.addEventListener('scroll', toggleBackToTopBtn);
+
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener('scroll', toggleBackToTopBtn);
+  }, []);
+
 
 
   return (
